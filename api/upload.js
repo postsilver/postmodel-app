1b80bdc2cd2cb05c2b6a3a0167c598ab9d48282e
postsilver <nikos.silver4@gmail.com>
2026-03-13 14:37:23 +0200

    fix: addRandomSuffix true, allowOverwrite false on mesh upload

--- api/upload.js
+++ api/upload.js
@@ -38,7 +38,8 @@ export default async function handler(req, res) {
             '',
           ],
           maximumSizeInBytes: 200 * 1024 * 1024,
-          addRandomSuffix: false,
+          addRandomSuffix: true,
+          allowOverwrite: false,
         };
       },
       onUploadCompleted: async ({ blob }) => {
